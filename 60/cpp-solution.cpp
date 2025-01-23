3ba9821e8df424b648f042ff73c3e730e05abb0c
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-23 17:17:32 -0600

    Update solution of question with ID "60"

--- 60/cpp-solution.cpp
+++ 60/cpp-solution.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+    inline char integerToChar(int n){
+        return n + '0';
+    }
+    int getFactorial(int n){
+        if (n == 0) return 1;
+        
+        return n * getFactorial(n-1);
+    }
     string getPermutation(int n, int k) {
-        cout << "peru is real" << endl;
+        int remainer = k - 1;
+        
+        vector<int> numbers(n);
+        iota(numbers.begin(), numbers.end(), 1);
+
+        string sequence = "";
+        
+        for(int i = n - 1; i >= 0; i--){
+            int factorial = getFactorial(i);
+            int index = floor(remainer/factorial);
+            remainer -= (factorial * index);
+        
+            sequence += integerToChar(numbers[index]);
+            numbers.erase(numbers.begin() + index);
+
+        }
+
+        return sequence;
     }
 };
