7e0678b2b285b7723475e2a5046c04c784102fed
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-24 17:40:23 -0600

    Update solution of question with ID "62"

--- 62/cpp-solution.cpp
+++ 62/cpp-solution.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        stack<pair<int,int>> remainer;
-        int uniquePaths = 0;
+        vector<int> dp(m + 1, 1);
+        dp[0] = 0;
 
-        remainer.push({0,0});
-
-        while (!remainer.empty()){
-            pair<int, int> currentCoordenates = remainer.top();
-            remainer.pop();
-
-            if (currentCoordenates.first < m - 1){
-                remainer.push({currentCoordenates.first + 1, currentCoordenates.second});
-            }
-            if (currentCoordenates.second < n - 1){
-                remainer.push({currentCoordenates.first, currentCoordenates.second + 1});
-            }
-            if (currentCoordenates.first >= m-1 && currentCoordenates.second >= n-1){
-                uniquePaths++;
+        for (int i = 1; i < n; i++){
+            for (int index = 1; index < dp.size(); index++){
+                dp[index] = dp[index] + dp[index-1];
             }
         }
 
-        return uniquePaths;
+        return dp.back();
     }
 };
