a44950e4a28f4b8be3e18b94b11dc1682767b54d
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-24 18:43:00 -0600

    Update solution of question with ID "63"

--- 63/cpp-solution.cpp
+++ 63/cpp-solution.cpp
@@ -8,6 +8,7 @@ public:
 
         vector<int> dp(n);
         bool isThereAnObstacle = false;
+        // map the first state of the dp variable in order to start identify any obstacle
         for (int i = 0; i < n; i++){
             isThereAnObstacle = isThereAnObstacle || obstacleGrid.front()[i] == 1;
 
@@ -18,9 +19,11 @@ public:
         for (int x = 1; x < m; x++){
             for (int y = 0; y < n; y++){
                 if (obstacleGrid[x][y] == 1){
+                    // in case there is an obstacle in the cell
                     dp[y] = 0;
                     continue;
                 }else if (y == 0){
+                    // in case it is the first of the row
                     continue;
                 }
 
