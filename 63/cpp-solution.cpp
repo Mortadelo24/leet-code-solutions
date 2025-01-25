34a5b1cc1f57aa753160acbd771a75ef28d09b0c
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-24 18:34:41 -0600

    Update solution of question with ID "63"

--- 63/cpp-solution.cpp
+++ 63/cpp-solution.cpp
@@ -8,28 +8,30 @@ public:
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         if (obstacleGrid[0][0] == 1 || obstacleGrid.back().back() == 1) return 0;
-        
+
         int m = obstacleGrid.size();
         int n = obstacleGrid.back().size();
 
-        vector<int> dp(n, 0);
+        vector<int> dp(n);
+        for (int i = 0; i < n; i++){
+            dp[i] = obstacleGrid.front()[i] == 1 ? 0 : 1; 
+            if(dp[i] == 0 && m == 1) return 0;
+        }
+        printState(dp);
 
-        for (int x = 0; x < m; x++){
-            for (int y = 0; y < n; y++){
+
+        for (int x = 1; x < m; x++){
+            for (int y = 1; y < n; y++){
                 if (obstacleGrid[x][y] == 1){
                     dp[y] = 0;
                     continue;
                 }
 
-                if (y == 0 || x == 0) {
-                    dp[y] = 1;
-                    continue;
-                }
-
-                dp[y] = dp[y] + dp[y-1];
+                dp[y] =  dp[y-1] + dp[y];
 
                 
             }
+            printState(dp);
             
         }
 
