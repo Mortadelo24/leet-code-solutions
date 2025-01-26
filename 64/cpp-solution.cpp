efdd241db50298ba8050f4a73ed130ffa80fe711
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-25 21:06:50 -0600

    Update solution for question with ID "64"

--- 64/cpp-solution.cpp
+++ 64/cpp-solution.cpp
@@ -10,11 +10,14 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         vector<int> state = vector(grid.front());
 
+        // fill the first row ignoring the first one and add the previous cell to the current
         for(int n = 1; n < state.size(); n++){
             state[n] = grid.front()[n] + state[n-1];
         }
 
+        // refill the row using the information already prossesed 
         for (int m = 1; m < grid.size(); m++) {
+            // fill the first cell of each row with the current state[m] + the first cell from the grid 
             state[0] = grid[m][0] + state[0];
 
             for (int n = 1; n < state.size(); n++) {
@@ -22,7 +25,6 @@ public:
                            (state[n - 1] < state[n] ? state[n - 1] : state[n]);
             }
 
-            printState(state);
         }
 
         return state.back();
