4899cfd3132de9a9353396f789525b90864988a5
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-25 20:57:07 -0600

    Update solution for question with ID "64"

--- 64/cpp-solution.cpp
+++ 64/cpp-solution.cpp
@@ -12,7 +12,7 @@ public:
         int tempIndex = 0;
         state[0] = grid[tempIndex][0] + state[0];
         for (int i = 1; i < state.size(); i++){
-            
+            state[i] = grid[tempIndex][i] + (state[i-1] < state[i] ? state[i-1] : state[i]);
         }
         printState(state);
 
