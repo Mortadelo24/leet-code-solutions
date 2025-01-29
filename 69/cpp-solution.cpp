71028913e801615271cd0c2cecb346760e6cb534
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-29 13:43:23 -0600

    Update solution for question with ID "69"

--- 69/cpp-solution.cpp
+++ 69/cpp-solution.cpp
@@ -5,13 +5,14 @@ public:
         int low = 0;
         int high = x;
         int result = 0;
+        unsigned long long square; 
 
         while (high - low > 1){
-            int mid = floor((low + high) / 2);
-            int pow = mid * mid;
-            if (x < pow){
+            unsigned long long mid = floor((low + high) / 2);
+            square = mid*mid;
+            if (square > x  ){
                 high = mid;
-            }else if(x >= pow){
+            }else if( square <= x ){
                 result = mid;
                 low = mid; 
             } else {
