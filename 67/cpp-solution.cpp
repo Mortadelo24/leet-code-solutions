625567276efb187973b861ea1f6b864a39dbe79f
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-28 20:25:39 -0600

    Update solution for question with ID "67"

--- 67/cpp-solution.cpp
+++ 67/cpp-solution.cpp
@@ -1,15 +1,49 @@
 class Solution {
 public:
+    string addBinary(char a, char b){
+        return addBinary(string(1,a), string(1,b));
+    }
     string addBinary(string a, string b) {
-        if (a.size() < b.size()) swap(a,b);
-        string result(a);
-
-        for (int i = 0; i < b.size(); i++ ){
-            int aNum = a[a.size()-1-i] == '1' ? 1 : 0;
-            int bNum = b[b.size()-1-i] == '1' ? 1 : 0;
-            cout << aNum << bNum << endl;
+        if (a.size() == 1 && b.size() == 1){
+            if (a[0] == '1' && b[0] == '1'){
+                return "10";
+            } else if (a[0] == '0' && b[0] == '0'){
+                return "0";
+            } 
+            return "1";
         }
+        if (a.size() < b.size()) swap(a,b);
+        string carry = "0";
 
-        return result;
-    } 
+        for (int i = 0; i < b.size(); i++){
+            int aIndex = a.size() - 1 - i;
+            int bIndex = b.size() - 1 - i;
+
+            string preResult = addBinary(addBinary(a[aIndex], b[bIndex]), carry);
+            carry = "0";
+
+
+            if (preResult.size() == 2){
+                carry = preResult.front();
+            }
+            a[aIndex] = preResult.back();
+        }
+        // if no carry is left
+        if (carry == "0") return a;
+
+        // if there is a carry but both strings have equal size
+        if (a.size() == b.size()){
+            a.insert(a.begin(), '1');
+        
+        } else {
+        // if there is a carry but both strings have difference size
+            string pre = addBinary(string(a.begin(), a.end() - b.size()), carry);
+            a.erase(a.begin(), a.end() - b.size());
+            a.insert(a.begin(), pre.begin(), pre.end());
+        }
+        
+        
+
+        return a;
+    }
 };
