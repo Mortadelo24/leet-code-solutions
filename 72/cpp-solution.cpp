a1845a86c994b8afa4d969e99296c0bede2b7e02
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 18:51:30 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        cout << "test" << endl;
+        if (word1.size() < word2.size()) swap(word1, word2);
+
+        vector<vector<optional<int>>> state = vector(word2.size(), vector(word1.size(), optional()));
+
+        
+
+
         return 0;
     }
 };
