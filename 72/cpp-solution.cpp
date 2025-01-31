288bf0084af30af463d012f1fef3df0f61af8c46
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 18:58:42 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    void printState(vector<vector<int*>>& state){
+        for (vector<int*> row : state){
+            for (int* cell : row){
+                if (!cell) cout << "N";
+                else cout << cell;
+                cout << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+
+    }
     int minDistance(string word1, string word2) {
         if (word1.size() < word2.size()) swap(word1, word2);
 
