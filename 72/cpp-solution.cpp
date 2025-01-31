caa5ce732ce85279611f2ceacfe86a9a9b6c84e3
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 19:27:53 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void printState(vector<vector<int*>>& state){
-        for (vector<int*> row : state){
-            for (int* cell : row){
+    void printState(vector<vector<int>>& state){
+        for (vector<int> row : state){
+            for (int cell : row){
                 if (!cell) cout << "N";
                 else cout << *cell;
                 cout << " ";
@@ -15,16 +15,13 @@ public:
     int minDistance(string word1, string word2) {
         if (word1.size() < word2.size()) swap(word1, word2);
 
-        vector<vector<int*>> state(word2.size(), vector(word1.size(), (int*) nullptr));
+        vector<vector<int>> state(word2.size(), vector(word1.size(), 0));
 
         for (int x = 0; x < word2.size(); x++){
             for(int y = 0; y < word1.size(); y++){
                 int distance = x-y;
-                int* currentCell = state[x][y];
-            
-                if (word2[x] == word1[y] && (!currentCell || distance < *currentCell) ){
-                    state[x][y] = new int(distance); 
-                }
+                int currentCell = state[x][y];
+                
             }
         }
         printState(state);
