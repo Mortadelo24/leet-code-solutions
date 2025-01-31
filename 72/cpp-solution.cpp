5bb25b4d05b2ad12e9a25c1422adceb4a42668ee
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 18:52:48 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -3,7 +3,7 @@ public:
     int minDistance(string word1, string word2) {
         if (word1.size() < word2.size()) swap(word1, word2);
 
-        vector<vector<int*>> state = vector(word2.size(), vector(word1.size(), nullptr));
+        vector<vector<int*>> state(word2.size(), vector(word1.size(), nullptr));
 
 
 
