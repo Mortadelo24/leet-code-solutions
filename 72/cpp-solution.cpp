c8f50d43bf79a49733758cc0d4043f1033ab13ea
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 20:49:50 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -17,8 +17,8 @@ public:
         for (int x = 1; x <= word1.size(); x++) state[x][0] = x;
         for (int y = 1; y <= word2.size(); y++) state[0][y] = y;
 
-        for (int x = 0; x <= word1.size(); x++){
-            for(int y = 0; y <= word2.size(); y++){
+        for (int x = 1; x <= word1.size(); x++){
+            for(int y = 1; y <= word2.size(); y++){
                 int base = min({state[x-1][y], state[x][y-1], state[x-1][y-1]});
                 
                 state[x][y] = word1[x-1] != word2[y-1] ? base + 1 : state[x-1][y-1];
