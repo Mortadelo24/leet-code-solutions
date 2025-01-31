3900783190b9b03ef59dd3bba618920d69da7931
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 17:32:38 -0600

    Update solution for question with ID "74"

--- 74/cpp-solution.cpp
+++ 74/cpp-solution.cpp
@@ -7,7 +7,7 @@ public:
         return 0;
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (int i = 0; i < matrix.size() + matrix.front().size(); i++){
+        for (int i = 0; i <= matrix.size() + matrix.front().size(); i++){
             getItem(matrix, i);
         }
         return false;
