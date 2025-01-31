548f257a2bddadcf180ecc6e9fa0fdcb57f65d4e
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 17:36:51 -0600

    Update solution for question with ID "74"

--- 74/cpp-solution.cpp
+++ 74/cpp-solution.cpp
@@ -8,7 +8,7 @@ public:
         // return matrix[floor( virtualIndex / n)][virtualIndex%n];
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (int i = 0; i <= matrix.size() * matrix.front().size(); i++){
+        for (int i = 0; i < matrix.size() * matrix.front().size(); i++){
             getItem(matrix, i);
         }
         return false;
