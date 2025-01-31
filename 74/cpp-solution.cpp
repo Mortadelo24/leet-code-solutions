7d0db19629b87564f2ee47ca9e5093d4215b1bb3
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 17:38:46 -0600

    Update solution for question with ID "74"

--- 74/cpp-solution.cpp
+++ 74/cpp-solution.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int getItem(vector<vector<int>>& matrix, int virtualIndex){
-        int n = matrix.front().size();
-        cout << virtualIndex/ n << "|" << virtualIndex%n << endl;
+    inline int getItem(vector<vector<int>>& matrix, int n,  int virtualIndex){
         return matrix[floor( virtualIndex / n)][virtualIndex%n];
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (int i = 0; i < matrix.size() * matrix.front().size(); i++){
-            cout << getItem(matrix, i) << endl;
+        int n = matrix.front().size();
+        for (int i = 0; i < matrix.size() * n; i++){
+            cout << getItem(matrix,n, i) << endl;
         }
         return false;
     }
