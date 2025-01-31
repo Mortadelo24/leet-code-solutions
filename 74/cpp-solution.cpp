cc0e71a1ae022d8ca7ce48a2f803f50ed964eea9
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 17:34:47 -0600

    Update solution for question with ID "74"

--- 74/cpp-solution.cpp
+++ 74/cpp-solution.cpp
@@ -6,7 +6,7 @@ public:
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for (int i = 0; i <= matrix.size() + matrix.front().size(); i++){
-            cout <<  getItem(matrix, i); << endl;
+            cout <<  getItem(matrix, i) << endl;
         }
         return false;
     }
