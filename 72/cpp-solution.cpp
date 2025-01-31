f44b6a817d8c6a85677b6e8472cb2fd646c5943e
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 20:49:09 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -14,18 +14,14 @@ public:
     int minDistance(string word1, string word2) {
         if (word1.size() > word2.size()) swap(word1, word2);
         vector<vector<int>> state(word1.size() + 1, vector(word2.size() + 1, 0));
-        for (int x = 0; x <= word1.size(); x++) state[x][0] = x;
+        for (int x = 1; x <= word1.size(); x++) state[x][0] = x;
+        for (int y = 1; y <= word2.size(); y++) state[0][y] = y;
+
         printState(state);
         return 0;
 
         for (int x = 0; x <= word1.size(); x++){
             for(int y = 0; y <= word2.size(); y++){
-                // fill initial state
-                if (y == 0){
-                    state[x][y] = x;
-                    continue;
-                }
-                // end
                 int base = min({state[x-1][y], state[x][y-1], state[x-1][y-1]});
                 
                 state[x][y] = word1[x-1] != word2[y-1] ? base + 1 : state[x-1][y-1];
