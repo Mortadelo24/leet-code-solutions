dd2985b03f47db973777ee1d22f4f1476a9a7906
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-30 20:50:35 -0600

    Update solution for question with ID "72"

--- 72/cpp-solution.cpp
+++ 72/cpp-solution.cpp
@@ -12,17 +12,16 @@ public:
 
     }
     int minDistance(string word1, string word2) {
-        if (word1.size() > word2.size()) swap(word1, word2);
+        // setup the grid
         vector<vector<int>> state(word1.size() + 1, vector(word2.size() + 1, 0));
         for (int x = 1; x <= word1.size(); x++) state[x][0] = x;
         for (int y = 1; y <= word2.size(); y++) state[0][y] = y;
+        // end
 
         for (int x = 1; x <= word1.size(); x++){
             for(int y = 1; y <= word2.size(); y++){
-                int base = min({state[x-1][y], state[x][y-1], state[x-1][y-1]});
-                
-                state[x][y] = word1[x-1] != word2[y-1] ? base + 1 : state[x-1][y-1];
-                            
+                int base = min({state[x-1][y], state[x][y-1], state[x-1][y-1]});        
+                state[x][y] = word1[x-1] != word2[y-1] ? base + 1 : state[x-1][y-1];         
             }
         }
 
