c2ccc90eb9e2d9c5112584ee93ea8d47279b195e
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 21:58:18 -0600

    Update solution for question with ID "75"

--- 75/cpp-solution.cpp
+++ 75/cpp-solution.cpp
@@ -20,7 +20,7 @@ public:
         }
         int i = 0;
         for (i; i < red; i++) nums[i] = 0;
-        for (i; i < white; i++) nums[i] = 1;
+        for (i; i < white; i++) cout << "a";
         for (i; i < blue; i++) nums[i] = 2;
 
     }
