9e7654fd642233356ab5c8b517ecb7186d10ebc8
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 21:38:34 -0600

    Update solution for question with ID "75"

--- 75/cpp-solution.cpp
+++ 75/cpp-solution.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             cout << " ";
             if (i == mark) {
-                cout << *;
+                cout << "^";
                 break;
             }
         } 
