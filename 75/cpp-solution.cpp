6694688c79f79130f9ecab2557a57f9d066d8f88
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 21:32:00 -0600

    Update solution for question with ID "75"

--- 75/cpp-solution.cpp
+++ 75/cpp-solution.cpp
@@ -5,27 +5,21 @@ public:
         int white = 0;
         int blue = 0;
 
-        auto getTotal = [nums, red, white, blue](int i) -> int {
+        for (int i = 0; i < nums.size(); i++){
             int total = 0;
             if (nums[i] == 0) total += red;
             if (nums[i] == 1) total += white;
             if (nums[i] == 2) total += blue;
-            return total;
-        };
-
-        for (int i = 0; i < nums.size(); i++){
-            int total = getTotal(i);
-
-            if (nums[i] == 0) red++;
-            else if (nums[i] == 1) white++;
-            else blue++;
 
             if (total != i){
                 cout << total << endl;
                 swap(nums[total], nums[i]);
-
-                
+                cout << nums[total] << " <-> " << nums[i] << endl;  
             }
+
+            if (nums[i] == 0) red++;
+            else if (nums[i] == 1) white++;
+            else blue++;
         }
 
     }
