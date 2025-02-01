75cf7272c90d54ea35594312027bf1772515d282
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 21:42:38 -0600

    Update solution for question with ID "75"

--- 75/cpp-solution.cpp
+++ 75/cpp-solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    void printState(vector<int>& nums, int mark = -1){
+    void printState(vector<int>& nums, set<int> marks){
         for (int i : nums) cout << i << " "; 
         cout << endl;
-        for (int i = 0; i < nums.size(); i++) {
-             if (i == mark) {
-                cout << "^";
-                break;
-            }
-            cout << "  ";
-           
-        } 
-        cout << endl;
+        string lineMarked(nums.size(), " "); 
+        for (int mark : marks){
+            lineMarked[i] = "^";
+        }
+        cout << lineMarked << endl;
     }
     void sortColors(vector<int>& nums) {
         int red = 0;
@@ -26,7 +22,7 @@ public:
 
             if (total != i){
                 cout << total << " <-> " << i << endl;  
-                printState(nums, i);
+                printState(nums, {total, i});
                 swap(nums[total], nums[i]);
             }
 
