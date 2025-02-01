efc91f460226ed8590fcf01b3bcd127ec13d1498
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-01-31 21:52:23 -0600

    Update solution for question with ID "75"

--- 75/cpp-solution.cpp
+++ 75/cpp-solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    void printState(vector<int>& nums){
-        for (int i : nums) cout << i << " "; 
+    void printState(vector<int>& nums) {
+        for (int i : nums)
+            cout << i << " ";
         cout << endl;
     }
     void sortColors(vector<int>& nums) {
@@ -9,22 +10,35 @@ public:
         int white = 0;
         int blue = 0;
 
-        for (int i = 0; i < nums.size(); i++){
-            int total = 0;
-            if (nums[i] == 0) total += red;
-            if (nums[i] == 1) total += white;
-            if (nums[i] == 2) total += blue;
-
-            if (total != i){
-                cout << total << " <-> " << i << endl;  
+        for (int i = 0; i < nums.size(); i++) {
+            while (true) {
+                int total = 0;
+                if (nums[i] == 0)
+                    total += red;
+                if (nums[i] == 1)
+                    total += white;
+                if (nums[i] == 2)
+                    total += blue;
+                cout << total << " <-> " << i << endl;
                 printState(nums);
                 swap(nums[total], nums[i]);
+
+                if (total == i) break;
+                if (nums[total] == 0)
+                    red--;
+                else if (nums[total] == 1)
+                    white--;
+                else
+                    blue--;
             }
 
-            if (nums[i] == 0) red++;
-            else if (nums[i] == 1) white++;
-            else blue++;
-        }
 
+            if (nums[i] == 0)
+                red++;
+            else if (nums[i] == 1)
+                white++;
+            else
+                blue++;
+        }
     }
 };
