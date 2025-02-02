43ea88a10e69b4c35bf6f7cacaa54071ab9aeeea
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-01 20:02:12 -0600

    Update solution for question with ID "76"

--- 76/cpp-solution.cpp
+++ 76/cpp-solution.cpp
@@ -8,12 +8,15 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char, int> state;
 
+        // map t in hash_map
         for (char i : t){
             if (state.find(i) == state.end()){
                 state[i] = 0;
             }
             state[i]++;
         }
+
+        printState(state);
         
 
         return "";
