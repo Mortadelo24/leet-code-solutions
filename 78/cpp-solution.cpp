b391b685bf5a521182764289ba50a43fc4fe7361
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-02 14:55:41 -0600

    Update solution for question with ID "78"

--- 78/cpp-solution.cpp
+++ 78/cpp-solution.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
-    void generateCombination(vector<vector<int>>combinations,vector<int>& nums, vector<int>& base, int start = 0){
+    void generateCombination(vector<vector<int>>& combinations,vector<int>& nums, vector<int>& base, int index = 0){
         combinations.push_back(base);
+        for (index; index < combinations.size(); index++){
+            cout << index << endl;
+        }
 
     }  
     vector<vector<int>> subsets(vector<int>& nums) {
