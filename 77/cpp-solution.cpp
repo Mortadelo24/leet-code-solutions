acd4a98666e8df40676991d5960c0874b5d0f589
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-02 14:37:24 -0600

    Update solution for question with ID "77"

--- 77/cpp-solution.cpp
+++ 77/cpp-solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> combine(int n, int k) {
-        vector<int> numbers(n);
-        vector<vector<int>> combinations;
-        iota(numbers.begin(), numbers.end(), 1);
-        
-        for (int i = 0; i < n - k + 1; i++){
-            combinations.push_back(vector(numbers.begin()+i, numbers.begin()+i + k));
-            
+    void generateCombinations(vector<vector<int>> combinations, int n, int k, vector<int> base = {}){
+        int start = base.empty() ? 0 : base.first();
+        for (int i = start; i <= n; i++){
+            cout << i << endl;
         }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> combinations;
+        
+        generateCombinations(combination, n, k, {});
 
 
         return combinations;
