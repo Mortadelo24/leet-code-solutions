fc0dedb8426123c97f4836d7e4890bb05928eaca
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-02 14:37:41 -0600

    Update solution for question with ID "77"

--- 77/cpp-solution.cpp
+++ 77/cpp-solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void generateCombinations(vector<vector<int>> combinations, int n, int k, vector<int> base = {}){
-        int start = base.empty() ? 0 : base.first();
+        int start = base.empty() ? 0 : base.back();
         for (int i = start; i <= n; i++){
             cout << i << endl;
         }
