56402cbae57882a57a1fd2b35bc3acbbc54f8fe8
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 17:27:25 -0600

    Update solution for question with ID "80"

--- 80/cpp-solution.cpp
+++ 80/cpp-solution.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         if(nums.size() < 2)return nums.size();
         
-        int last = 1;
+        int last = 2;
         for(int i = 2; i < nums.size(); i++){
             if(nums[last-2]!=nums[i])
             {
