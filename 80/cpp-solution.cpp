539e69c6c14d180a3419aeed8d432f53b9084ca0
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 17:20:47 -0600

    Update solution for question with ID "80"

--- 80/cpp-solution.cpp
+++ 80/cpp-solution.cpp
@@ -4,8 +4,7 @@ public:
         bool isSecond = false;
         int last = 0;
         for(int i = 1; i < nums.size(); i++){
-            if (isSecond && nums[i] == nums[last])continue;
-            if (nums[last] == nums[i]) isSecond = true;
+            if (nums[i] == nums[last])continue;
             nums[++last] = nums[i];
         }
 
