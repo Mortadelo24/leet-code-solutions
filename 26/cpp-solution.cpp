f3a2e215d4cc3dd29bd371fdfbd405c9b652bf27
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 15:32:35 -0600

    Update solution for question with ID "26"

--- 26/cpp-solution.cpp
+++ 26/cpp-solution.cpp
@@ -4,6 +4,9 @@ public:
         for(int i = 0; i < nums.size(); i++){
             nums.erase(nums.begin() + i);
         }
+        for (int i : nums){
+            cout << i << endl;
+        }
 
         return nums.size();
     }
