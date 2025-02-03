923cb40a2929b50816c3528b12561aaed9ac9bbe
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 13:38:57 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -42,17 +42,15 @@ public:
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
+        if (word.size() > board.size() * board.front().size()) return false;
         vector<pair<int, int>> state;
 
         for (int i = 0; i < board.size(); i++){
             for (int j = 0; j < board.front().size(); j++){
-                if (board[i][j] == word.front()){
-                    state.push_back({i,j});
-                    if (solveWord(board, word, state)) return true;
-                    state.pop_back();
-                }
+                
             }
         }
+        
 
         return false;
     }
