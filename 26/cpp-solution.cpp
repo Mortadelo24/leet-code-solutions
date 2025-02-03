ade1f52fe75ea212eca7de1203d2ea0fc54da9c4
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 15:28:28 -0600

    Update solution for question with ID "26"

--- 26/cpp-solution.cpp
+++ 26/cpp-solution.cpp
@@ -3,8 +3,8 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int currentNum = nums.front();
         int previous = 1;
-        for(int i = 1; i < nums.size(); i++){
-            previous = nums[i]
+        for(int i = 0; i < nums.size(); i++){
+            previous = nums[i];
             nums.erase(nums.begin() + i);
         }
 
