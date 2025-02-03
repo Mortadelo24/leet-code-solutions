028ad397c8dab176139d0cfca6fd3feb938e8ef1
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 12:10:54 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -10,7 +10,7 @@ public:
             for (int j = boardCoordenates.second == 0 ? 0 : boardCoordenates.second  - 1; j <= boardCoordenates.second + 1 && j < board.front().size(); j++){
                 pair<int, int> tempMove = {i,j};
                 if (tempMove == boardCoordenates) continue;
-                cout << includesMove(state, tempMove) << " ";
+                cout << !includesMove(state, tempMove) && board[i][j] == word[wordIndex] << " ";
             }
         }
         cout << endl;
