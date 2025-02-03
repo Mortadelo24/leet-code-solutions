fc26f89b1fa7f427873b22e0716131ce1c199be3
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 17:20:00 -0600

    Update solution for question with ID "80"

--- 80/cpp-solution.cpp
+++ 80/cpp-solution.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        bool isSecond;
+        bool isSecond = false;
         int last = 0;
         for(int i = 1; i < nums.size(); i++){
             if (isSecond && nums[i] == nums[last])continue;
+            if (nums[last] == nums[i]) isSecond = true;
             nums[++last] = nums[i];
         }
 
