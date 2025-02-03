a6638dd7f348588a6267c1f8abbbf9b8ebc75a99
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 11:39:25 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -15,8 +15,9 @@ public:
             for (int j = 0; j < board.front().size(); j++){
                 if (board[i][j] == word.front()){
                     set<pair<int, int>> state;
-                    state.insert({i, j});
-                    solveWord(board, word, state, {i,j},  1);
+                    pair<int, int> coordenates = {i,j};
+                    state.insert(coordenates);
+                    solveWord(board, word, state, coordenates,  1);
                 }
             }
         }
