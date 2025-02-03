a10d1629cc47e961c7dcef9ad9d1a229770e5556
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 11:27:04 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    bool solveWord(vector<vector<char>>& board, string& word, unordered_set<pair<int, int>>& members){
+    bool solveWord(vector<vector<char>>& board, string& word, stack<pair<int, int>>& members, pair<int, int> coordenates){
+
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
         for (int i = 0; i < board.size(); i++){
             for (int j = 0; j < board.front().size(); j++){
                 if (board[i][j] == word.front()){
-                    unordered_set<pair<int, int>> members;
-                    solveWord(board, word, members);
+                    stack<pair<int, int>> members;
+                    solveWord(board, word, members, {i,j});
                 }
             }
         }
