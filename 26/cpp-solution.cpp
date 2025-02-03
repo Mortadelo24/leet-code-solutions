65684c0474d684dba174e3bcf138deb2294e6e79
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 15:23:58 -0600

    Update solution for question with ID "26"

--- 26/cpp-solution.cpp
+++ 26/cpp-solution.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int currentNum = nums.front();
-        int uniqueElements = 0;
+        int uniqueElements = 1;
         for(int i = 1; i < nums.size(); i++){
             if (nums[i] != currentNum){
                 currentNum = nums[i];
+                uniqueElements++;
                 continue;
             }
             nums.erase(nums.begin() + i);
