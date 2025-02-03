f44c99cffb7cbad0baa0f5efc018dddeda1b9056
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 11:28:08 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solveWord(vector<vector<char>>& board, string& word, stack<pair<int, int>>& members, pair<int, int> coordenates){
+    bool solveWord(vector<vector<char>>& board, string& word, stack<pair<int, int>>& state){
 
         return false;
     }
@@ -8,8 +8,9 @@ public:
         for (int i = 0; i < board.size(); i++){
             for (int j = 0; j < board.front().size(); j++){
                 if (board[i][j] == word.front()){
-                    stack<pair<int, int>> members;
-                    solveWord(board, word, members, {i,j});
+                    stack<pair<int, int>> state;
+                    state.push({i, j});
+                    solveWord(board, word, state);
                 }
             }
         }
