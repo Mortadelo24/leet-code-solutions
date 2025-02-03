6147bf8f8e32a4a702828c95696c403a0ba8bddc
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 12:26:55 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -4,10 +4,7 @@ public:
         return state.find(move) != state.end();        
     }
     bool solveWord(vector<vector<char>>& board, string& word, set<pair<int, int>>& state, pair<int, int> boardCoordenates, int wordIndex){    
-        cout << state.size() << endl; 
-        if (state.size() == word.size()){
-            return true;
-        }  
+        if (word.size() == state.size()) return true;
         for (int i = boardCoordenates.first == 0 ? 0 : boardCoordenates.first  - 1; i <= boardCoordenates.first + 1 && i < board.size(); i++ ){
             for (int j = boardCoordenates.second == 0 ? 0 : boardCoordenates.second  - 1; j <= boardCoordenates.second + 1 && j < board.front().size(); j++){
                 pair<int, int> tempMove = {i,j};
@@ -25,6 +22,8 @@ public:
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
+        set<pair<int, int>> state;
+
         for (int i = 0; i < board.size(); i++){
             for (int j = 0; j < board.front().size(); j++){
                 if (board[i][j] == word.front()){
