1e906b9a39579f828fb7a4aead3f8a9df89810b2
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 15:36:35 -0600

    Update solution for question with ID "26"

--- 26/cpp-solution.cpp
+++ 26/cpp-solution.cpp
@@ -3,10 +3,11 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int index = 0;
         while(index < nums.size()){
-            if (index>0 && nums[index] == nums[index - 1]){
+            if (index > 0 && nums[index] == nums[index - 1]){
                 nums.erase(nums.begin() + index);
                 index--;
             }
+            cout << "a" << endl;
         }
 
         for (int i : nums){
