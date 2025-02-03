393d65c1aa6bf3315566a402e248caf1a83ee105
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 11:22:33 -0600

    Update solution for question with ID "79"

--- 79/cpp-solution.cpp
+++ 79/cpp-solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
+    bool solveWord(vector<vector<char>>& board, string& word, unordered_set<pair<int, int>>& members){
+        return false;
+    }
     bool exist(vector<vector<char>>& board, string word) {
         for (int i = 0; i < board.size(); i++){
             for (int j = 0; j < board.front().size(); j++){
                 if (board[i][j] == word.front()){
-                    cout << "start searching" << endl;
+                    unordered_set<pair<int, int>> members;
+                    solveWord(board, word, members);
                 }
             }
         }
