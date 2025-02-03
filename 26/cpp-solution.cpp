6ba79204cf925e2add362d4aee3e9a40bab0d0e1
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 15:41:51 -0600

    Update solution for question with ID "26"

--- 26/cpp-solution.cpp
+++ 26/cpp-solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int index = 0;
+        int index = 1;
         int lastUnique = 0;
         while(index < nums.size()){
             if (nums[index] != nums[lastUnique]){
