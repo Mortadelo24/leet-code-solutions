da03d89d30063335aac98620a5aee2feb6ab07be
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-03 15:32:11 -0600

    Update solution for question with ID "26"

--- 26/cpp-solution.cpp
+++ 26/cpp-solution.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int currentNum = nums.front();
-        int previous = 1;
         for(int i = 0; i < nums.size(); i++){
-            previous = nums[i];
             nums.erase(nums.begin() + i);
         }
 
-        for (int a : nums){
-            cout << a << endl;
-        }
-        return 0;
+        return nums.size();
     }
 };
