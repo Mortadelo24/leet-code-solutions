be62b7604a0a31816220370276829466f1f9ce2b
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-04 14:20:25 -0600

    Add solution for question with ID "82"

---
+++ 82/cpp-solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        ListNode* tailNode = head;  
+        ListNode* currentNode = head->next;  
+
+        while (currentNode != nullptr){
+            if (currentNode->val != tailNode->val){
+                tailNode->next = currentNode;
+                tailNode = currentNode;
+            }
+            currentNode = currentNode->next;
+        }
+        tailNode->next = nullptr;
+
+        return head;
+    }
+};
