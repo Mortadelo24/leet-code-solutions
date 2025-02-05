837787bdde985d1ee2841f1d1d47a7d5cf5a8272
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-04 18:02:20 -0600

    Update solution for question with ID "84"

--- 84/cpp-solution.cpp
+++ 84/cpp-solution.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        stack<int> candidates;
+        for (int i = 0; i < heights.size(); i++){
+            while(!candidates.empty() && heights[candidates.top()] < heights[i]) candidates.pop();
+            candidates.push(i);
+        }
         return 0;
     }
 };
