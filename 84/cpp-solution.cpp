7b073239290487e57f50f29038a7415ca75a9d35
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-04 18:05:47 -0600

    Update solution for question with ID "84"

--- 84/cpp-solution.cpp
+++ 84/cpp-solution.cpp
@@ -9,7 +9,7 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> candidates;
         for (int i = 0; i < heights.size(); i++){
-            while(!candidates.empty() && heights[candidates.top()] < heights[i]) candidates.pop();
+            while(!candidates.empty() && heights[candidates.top()] > heights[i]) candidates.pop();
             candidates.push(i);
         }
         printStack(candidates);
