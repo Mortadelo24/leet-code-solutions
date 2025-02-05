ef90ad2c42464eb93a6fa152e90e5a7bb7ecafe5
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-04 18:51:38 -0600

    Update solution for question with ID "84"

--- 84/cpp-solution.cpp
+++ 84/cpp-solution.cpp
@@ -12,6 +12,7 @@ public:
         for (int i = 0; i < heights.size(); i++){
             // insert element in stack
             while(!candidates.empty() && heights[candidates.back()] > heights[i]) candidates.pop_back();
+            if (!candidates.empty() && heights[candidates.back()] == heights[i]) continue;
             candidates.push_back(i);
             // end
             // printStack(candidates, heights);
