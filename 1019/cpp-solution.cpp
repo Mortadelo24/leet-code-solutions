0621579f406d33fecc9fd98583b9848cc7163a49
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-07 15:48:03 -0600

    Update solution for question with ID "1019"

--- 1019/cpp-solution.cpp
+++ 1019/cpp-solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> ans; 
+        vector<int> ans(nums.size()); 
+        int i = nums.size() - 1;
 
         int left = 0;
         int right = nums.size() - 1;
 
         while(left <= right){
             if (abs(nums[left]) > abs(nums[right])){
-                ans.insert(ans.begin(), pow(nums[left], 2));
+                ans[i--] = pow(nums[left], 2);
                 left++;
             } else {
-                ans.insert(ans.begin(), pow(nums[right], 2));
+                ans[i--] = pow(nums[right], 2);
                 right--;
             }
         }
