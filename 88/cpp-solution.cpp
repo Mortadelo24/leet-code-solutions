a1da6069f91d35b837e39a4490d004ea6a922662
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-07 14:42:04 -0600

    Update solution for question with ID "88"

--- 88/cpp-solution.cpp
+++ 88/cpp-solution.cpp
@@ -5,11 +5,14 @@ public:
         int index = 0;
 
         nums1.erase(nums1.end() - n,nums1.end());
-        
-        for (int i = 0; i < m; i++){
-            if (nums1[i] > nums2[index]) nums1.insert(nums1.begin()+i, nums2[index++]);
+
+        for (int i = 0; i < m + n; i++){
+            if (nums1[i] <= nums2[index]) continue;
+            nums1.insert(nums1.begin()+i, nums2[index++]);
+            i--;
         }
 
         nums1.insert(nums1.end(), nums2.begin() + index, nums2.end());
+
     }
 };
