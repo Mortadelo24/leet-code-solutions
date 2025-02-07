da4a927b822f8b2a994d1c12ee673e9c7ddf8edf
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-07 14:29:23 -0600

    Add solution for question with ID "88"

---
+++ 88/cpp-solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (nums2.size() < 1) return;
+        int index = 0;
+
+        nums1.erase(nums1.end() - n,nums1.end());
+        
+        for (int i = 0; i < m; i++){
+            if (nums1[i] > nums2[index]) nums1.insert(nums1.begin()+i, nums2[index++]);
+        }
+
+        nums1.insert(nums1.end(), nums2.begin() + index, nums2.end());
+    }
+};
