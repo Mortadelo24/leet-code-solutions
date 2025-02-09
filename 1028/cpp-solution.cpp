0474927b1c3736ea1ccf3ccbe634177782217a55
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-08 20:03:24 -0600

    Update solution for question with ID "1028"

--- 1028/cpp-solution.cpp
+++ 1028/cpp-solution.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-    bool collides(vector<int>& rangeA, vector<int>& rangeB){
+    bool oneSidedCollide(vector<int>& rangeA, vector<int>& rangeB){
         return (rangeA[0] >= rangeB[0] && rangeA[0] <= rangeB[1]) || (rangeA[1] >= rangeB[0] && rangeA[1] <= rangeB[1]);
     }
+    bool collides(vector<int>& rangeA, vector<int>& rangeB){
+        return oneSidedCollide(rangeA, rangeB) || oneSidedCollide(rangeB, rangeA);
+    }
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        if (firstList.size() < secondList.size()) swap(firstList, secondList);
         vector<vector<int>> ranges;
 
         int pointerA = 0;
@@ -13,16 +17,16 @@ public:
             vector<int>& rangeA = firstList[pointerA];
             vector<int>& rangeB = secondList[pointerB];
             if (collides(rangeA, rangeB)){
-                int subPointerB = pointerB;
-                while(subPointerB < secondList.size() && (collides(rangeA,  secondList[subPointerB]) || collides( secondList[subPointerB], rangeA) )){
-                    ranges.push_back({max(rangeA[0], secondList[subPointerB][0]), min(rangeA[1], secondList[subPointerB][1])});
-                    subPointerB++;
+                int subPointerA = pointerA;
+                while(subPointerA < firstList.size() && collides(rangeB,  firstList[subPointerA])){
+                    ranges.push_back({max(firstList[subPointerA][0], rangeB[0]), min(firstList[subPointerA][1], rangeB[1])});
+                    subPointerA++;
                 }
-             
-                pointerA++;
-            }else {
+
                 pointerB++;
             }
+
+            pointerA++;
         }
 
         return ranges;
