824258c28b15fcfd9d6e91142dfb42ef35d4513e
José Miranda <111203398+Mortadelo24@users.noreply.github.com>
2025-02-10 19:10:26 -0600

    Update solution for question with ID "717"

--- 717/cpp-solution.cpp
+++ 717/cpp-solution.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
-        for (int i : bits){
-            if (i == 1) return bits.size() % 2 != 0;
-        }
-        return true;
+        return bits[bits.size()-2] == 0;
     }
 };
